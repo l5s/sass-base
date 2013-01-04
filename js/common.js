ad7d8fa8c631080f8d6236424bbc6e91457a10fe
Alex Martens <alex.martens@icloud.com>
2013-01-04 09:33:58 -0600

    first commit

---
+++ js/common.js
@@ -0,0 +1,42 @@
+/*
+	AVOID CONSOLE ERRORS IN IE - HTML5 BOILERPLATE
+*/
+
+(function() {
+    var method;
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+/*
+	DOM READY
+*/
+
+$(function () {
+	
+	var primaryNav = $('#primary-nav');
+	
+	$('#toggle-menu').click(function () {
+		
+		primaryNav.slideToggle();
+		
+		return false;
+	});
+	
+});
